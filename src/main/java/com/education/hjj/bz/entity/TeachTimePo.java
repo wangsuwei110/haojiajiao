1ced247d27fbc44df4bb862e3a4b48f591eb676f
wangsuwei110 <2509688878@qq.com>
2019-09-21 23:52:19 +0800

    增加中间teachTime转换类

---
+++ src/main/java/com/education/hjj/bz/entity/TeachTimePo.java
@@ -0,0 +1,43 @@
+package com.education.hjj.bz.entity;
+
+import java.io.Serializable;
+
+public class TeachTimePo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1796219611751813099L;
+	
+	private String week;
+	
+	private String time;
+
+	public String getWeek() {
+		return week;
+	}
+
+	public void setWeek(String week) {
+		this.week = week;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public TeachTimePo(String week, String time) {
+		super();
+		this.week = week;
+		this.time = time;
+	}
+
+	public TeachTimePo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
