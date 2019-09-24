19ce8fd1d92b367ce5688b326c48a07a9f1b7426
wangsuwei110 <2509688878@qq.com>
2019-09-24 23:43:32 +0800

    增加教员账户的查询

--- src/main/java/com/education/hjj/bz/controller/ComplaintSuggestionController.java
+++ src/main/java/com/education/hjj/bz/controller/ComplaintSuggestionController.java
@@ -2,6 +2,8 @@ package com.education.hjj.bz.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,8 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping(value = "/ComplaintSuggestion")
 public class ComplaintSuggestionController {
+	
+	private static Logger logger = LoggerFactory.getLogger(ComplaintSuggestionController.class);
 
 	@Autowired
 	private ComplaintSuggestionService complaintSuggestionService;
