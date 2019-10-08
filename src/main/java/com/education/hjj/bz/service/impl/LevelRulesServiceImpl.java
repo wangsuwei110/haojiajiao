bb7df977b6fc8f38d6f0d1f39f72fb4838f55e89
wangsuwei110 <2509688878@qq.com>
2019-10-08 23:06:03 +0800

    更新教员等级查询的方法

--- src/main/java/com/education/hjj/bz/service/impl/LevelRulesServiceImpl.java
+++ src/main/java/com/education/hjj/bz/service/impl/LevelRulesServiceImpl.java
@@ -7,20 +7,25 @@ import org.springframework.stereotype.Service;
 
 import com.education.hjj.bz.entity.vo.PageVo;
 import com.education.hjj.bz.entity.vo.TeacherLevelRuleVo;
+import com.education.hjj.bz.mapper.LevelRulesMapper;
 import com.education.hjj.bz.service.LevelRulesService;
 
 @Service
 public class LevelRulesServiceImpl implements LevelRulesService{
 	
 	@Autowired
-	private LevelRulesService LevelRulesService;
+	private LevelRulesMapper levelRulesMapper;
 
 	@Override
 	public PageVo<List<TeacherLevelRuleVo>> queryAllLevelRules() {
 		
-		PageVo<List<TeacherLevelRuleVo>> list = LevelRulesService.queryAllLevelRules();
+		List<TeacherLevelRuleVo> list = levelRulesMapper.queryAllLevelRules();
+		
+		PageVo page = new PageVo();
+		page.setTotal(list.size());
+		page.setDataList(list);
 
-		return list;
+		return page;
 	}
 
 	
