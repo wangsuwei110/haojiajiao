b4ec1d5cd8bee3e6ed1a626c2e31df0841a8336e
wangsuwei110 <2509688878@qq.com>
2019-11-20 23:30:44 +0800

    新增获取教员年级的方法

--- src/main/java/com/education/hjj/bz/controller/GradeController.java
+++ src/main/java/com/education/hjj/bz/controller/GradeController.java
@@ -1,13 +1,19 @@
 package com.education.hjj.bz.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.education.hjj.bz.entity.vo.TeachGradeVo;
 import com.education.hjj.bz.service.GradeService;
 import com.education.hjj.bz.util.ApiResponse;
 import io.swagger.annotations.Api;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.education.hjj.bz.service.TeachGradeService;
 
 /**
  * 年级常量表控制类
@@ -23,6 +29,9 @@ public class GradeController {
 
     @Autowired
     private GradeService gradeService;
+    
+    @Autowired
+    private TeachGradeService teachGradeService;
 
    // 查询
 	@PostMapping("/list")
@@ -30,5 +39,16 @@ public class GradeController {
 	public ApiResponse list() {
 		return gradeService.listAllGrade();
     }
+	
+	
+	@PostMapping("/queryAllGradelist")
+	@ResponseBody
+	public ApiResponse queryAllGradelist() {
+		
+		List<TeachGradeVo> list = teachGradeService.queryAllTeachGrade();
+		
+		return ApiResponse.success("操作成功", JSON.toJSON(list));
+    }
+	
 
 }
