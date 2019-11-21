9c8d63213a1e1382293c50123585e55b590bd23d
wangsuwei110 <2509688878@qq.com>
2019-11-22 00:59:12 +0800

    修复缺陷

---
+++ src/main/java/com/education/hjj/bz/service/StudentDemandConnectService.java
@@ -0,0 +1,15 @@
+package com.education.hjj.bz.service;
+
+import java.util.List;
+
+import com.education.hjj.bz.entity.vo.StudentDemandConnectVo;
+import com.education.hjj.bz.entity.vo.StudentDemandVo;
+
+public interface StudentDemandConnectService {
+	
+	int queryServiceForStudentSuccess(Integer teacherId);
+	
+    List<StudentDemandConnectVo> queryStudentAppraiseForTeacher(Integer teacherId);
+    
+    List<StudentDemandVo> queryServiceForStudentByTeacherId(Integer teacherId);
+}
