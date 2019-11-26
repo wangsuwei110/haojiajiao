9e3615de460bfdafc0262061436f1007d1e17dcc
wangsuwei110 <2509688878@qq.com>
2019-11-27 00:45:20 +0800

    修改数据类型

--- src/main/java/com/education/hjj/bz/controller/WxRedPackController.java
+++ src/main/java/com/education/hjj/bz/controller/WxRedPackController.java
@@ -124,11 +124,13 @@ public class WxRedPackController {
 		BigDecimal  rebateData = new BigDecimal(100 * 0.95);//转化为分
 		
 		
-		BigDecimal cash_out = cashOutData.multiply(rebateData).setScale(0, BigDecimal.ROUND_HALF_UP);//实际到账金额（分）
+		int cash_out = cashOutData.multiply(rebateData).intValue();//实际到账金额（分）
 		logger.info("当前用户要提现的金额,转化为分: " + cash_out +" 分");
 		
-		BigDecimal cash = cash_out.divide(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_HALF_UP);//实际到账金额（元）
+		BigDecimal cash = new BigDecimal(cash_out).divide(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_HALF_UP);//实际到账金额（元） 
 		logger.info("当前用户要提现的金额,转化为元: " + cashOutData +" 元");
+		
+		logger.info("当前用户实际到账金额: " + cash +" 元");
 		
 		BigDecimal commission_cash = cashOutData.subtract(cash).setScale(2, BigDecimal.ROUND_HALF_UP);//手续费金额（元）
 		logger.info("当前用户要提现的金额手续费: " + commission_cash +" 元");
