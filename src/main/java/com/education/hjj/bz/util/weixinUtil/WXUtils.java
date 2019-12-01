93bca254439d0612feec6d6b598b1af58c2e8f45
caohuan2092 <513150051@qq.com>
2019-12-01 15:39:07 +0800

    支付时，状态为7，续课时

--- src/main/java/com/education/hjj/bz/util/weixinUtil/WXUtils.java
+++ src/main/java/com/education/hjj/bz/util/weixinUtil/WXUtils.java
@@ -226,19 +226,21 @@ public class WXUtils {
 
     /**  
      * 签名字符串  
-     *  @param text需要签名的字符串  
+     *  @param text 需要签名的字符串
      * @param sign 签名结果  
-     * @param key密钥  
+     * @param key 密钥
      * @param input_charset 编码格式  
      * @return 签名结果  
      */   
     public static boolean verify(String text, String sign, String key, String input_charset) {   
         text = text + key;   
         String mysign = DigestUtils.md5Hex(getContentBytes(text, input_charset));   
-        if (mysign.equals(sign)) {   
-            return true;   
-        } else {   
-            return false;   
+        if (mysign.equals(sign)) {
+        	logger.info("caohuan****************验证成功");
+            return true;
+		} else {
+			logger.info("caohuan****************验证失败");
+			return false;
         }   
     } 
     
