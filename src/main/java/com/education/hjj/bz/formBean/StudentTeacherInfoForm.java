549ac16bb5af37d8c4ec61f243dfd7080ffde05c
wangsuwei110 <2509688878@qq.com>
2019-12-04 22:16:32 +0800

    增加学员端查找教员时的类

---
+++ src/main/java/com/education/hjj/bz/formBean/StudentTeacherInfoForm.java
@@ -0,0 +1,83 @@
+package com.education.hjj.bz.formBean;
+
+import java.io.Serializable;
+
+public class StudentTeacherInfoForm extends PageForm implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9147674499346990621L;
+	
+	private Integer teachBrance;
+	
+	private Integer teachAddress;
+	
+	private String school;
+	
+	private Integer isGraduate;
+
+	private Integer teacherPoints;
+	
+	private String createTime;
+	
+	private Integer sex;
+
+	public Integer getTeachBrance() {
+		return teachBrance;
+	}
+
+	public void setTeachBrance(Integer teachBrance) {
+		this.teachBrance = teachBrance;
+	}
+
+	public Integer getTeachAddress() {
+		return teachAddress;
+	}
+
+	public void setTeachAddress(Integer teachAddress) {
+		this.teachAddress = teachAddress;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public Integer getIsGraduate() {
+		return isGraduate;
+	}
+
+	public void setIsGraduate(Integer isGraduate) {
+		this.isGraduate = isGraduate;
+	}
+
+	public Integer getTeacherPoints() {
+		return teacherPoints;
+	}
+
+	public void setTeacherPoints(Integer teacherPoints) {
+		this.teacherPoints = teacherPoints;
+	}
+
+	public String getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+
+	public Integer getSex() {
+		return sex;
+	}
+
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+	
+	
+}
