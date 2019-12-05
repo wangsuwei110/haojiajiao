36eecd4e64e61006f2d82fa6918ff5162e45a7bf
wangsuwei110 <2509688878@qq.com>
2019-12-06 01:11:43 +0800

    增加日志打印

--- src/main/java/com/education/hjj/bz/service/impl/StudentDemandsServiceImpl.java
+++ src/main/java/com/education/hjj/bz/service/impl/StudentDemandsServiceImpl.java
@@ -1144,6 +1144,10 @@ if(sdcList.size() > 0 && list.size() > 0 ) {
 	@Override
 	public Map<String , Object> validateSignParameters(StudentDemandConnectForm demandForm) {
 		
+		logger.info("报名的教员id:{},订单id:{},前端传值--授课科目id:{} , 授课年级id: {} , 授课区域id{} , 授课时间{}" ,
+				demandForm.getTeacherId() , demandForm.getDemandId() , demandForm.getTeachBranchId() , demandForm.getTeachGradeId()
+				,demandForm.getParameterId() , JSON.toJSONString(demandForm.getTimeList()));
+		
 		boolean flag = true;
 		
 		boolean timeFlag = false;
